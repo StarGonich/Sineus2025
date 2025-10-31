a80d49c474dbc8c40f7519bfb6654846c25e2a47
Alexey Velichayshiy <alexey8837@gmail.com>
2025-11-01 00:51:05 +0300

    PlayGameButton

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scenes/SampleScene");
+    }
+
     public void ExitGame() {
         Debug.Log("Закрытие игры");
         Application.Quit();
