83ee7e6abefdde43ee0c60c60c4e3012c970ea75
Alexey Velichayshiy <alexey8837@gmail.com>
2025-11-01 17:23:25 +0300

    add cursor and logo

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Scenes/SampleScene");
+        SceneManager.LoadScene("Scenes/MainScene");
     }
 
     public void ExitGame() {
